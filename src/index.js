d447b4863ad6b0e1e2049c43a60ef35938ba2471
shannon_p <snp1284kasey@aol.com>
2021-10-26 19:45:19 -0400

    created an robofriends cards

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "tachyons"; //similar to Bootstrap
 
 ReactDOM.render(
   <React.StrictMode>
